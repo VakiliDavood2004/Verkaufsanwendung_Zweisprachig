cb61ed9b938f9e71b741748aa1ca8022aa9cdd75
VakiliDavood2004 <vakilidavood2004@gmail.com>
2024-04-01 10:00:00 +0330

    Kundendatenstruktur initial definiert

---
+++ DE-Verkaufs-Anwendung/customer_module.py
@@ -0,0 +1,27 @@
+import sys
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QWidget, QFormLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
+
+class CustomerForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Kunden registrieren")
+        self.resize(400, 250)
+        self.init_db()
+        form_layout = QFormLayout()
+        self.name_input = QLineEdit()
+        self.phone_input = QLineEdit()
+        self.address_input = QLineEdit()
+
+        form_layout.addRow("Kundenname:", self.name_input)
+        form_layout.addRow("Telefonnummer:", self.phone_input)
+        form_layout.addRow("Adresse:", self.address_input)
+
+        submit_button = QPushButton("Kunden registrieren")
+        submit_button.clicked.connect(self.submit_data)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(form_layout)
+        main_layout.addWidget(submit_button)
+        self.setLayout(main_layout)
+
