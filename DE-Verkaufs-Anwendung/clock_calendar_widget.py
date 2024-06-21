7cf9854ed4e50151e533bca01f1d02a6c9b1924c
VakiliDavood2004 <vakilidavood2004@gmail.com>
2024-06-21 15:00:00 +0330

    Widget final optimiert

--- DE-Verkaufs-Anwendung/clock_calendar_widget.py
+++ DE-Verkaufs-Anwendung/clock_calendar_widget.py
@@ -18,6 +18,7 @@ class AnalogClock(QWidget):
         center_y = self.height() / 2
         painter.translate(center_x, center_y)
         painter.scale(self.width() / 200.0, self.height() / 200.0)
+
         # Zeichnen von Uhrmarkierungen
         painter.setPen(QPen(Qt.black, 2))
         for i in range(12):
@@ -44,3 +45,22 @@ class AnalogClock(QWidget):
         hour_angle = (current_time.hour() % 12 + current_time.minute() / 60) * 30 * math.pi / 180
         painter.setPen(QPen(Qt.black, 5))
         painter.drawLine(0, 0, int(40 * math.cos(hour_angle)), int(40 * math.sin(hour_angle)))
+
+class CalendarLabel(QLabel):
+    def __init__(self):
+        super().__init__()
+        self.setAlignment(Qt.AlignCenter)
+        self.setStyleSheet("""
+            font-size: 14px;
+            color: #2c3e50;
+            font-weight: bold;
+        """)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_text)
+        self.timer.start(1000)
+        self.update_text()
+
+    def update_text(self):
+        now = QDateTime.currentDateTime()
+        self.setText(now.toString("dddd - yyyy/MM/dd"))
