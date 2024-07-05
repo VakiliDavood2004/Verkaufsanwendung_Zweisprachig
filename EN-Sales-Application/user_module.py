ff968dcaf213ceee7d7cb992507ba8b426ac3c43
VakiliDavood2004 <vakilidavood2004@gmail.com>
2024-07-05 15:00:00 +0330

    Benutzermodul final überarbeitet AUF Englisch

--- EN-Sales-Application/user_module.py
+++ EN-Sales-Application/user_module.py
@@ -10,19 +10,19 @@ class User_Form(QWidget):
         self.initDB()
 
     def initUI(self):
-        self.setWindowTitle("Benutzername und Passwort")
+        self.setWindowTitle("Username And password")
         self.setGeometry(100, 100, 300, 200)
 
         layout = QVBoxLayout()
 
-        self.label1 = QLabel("Benutzername:")
+        self.label1 = QLabel("Username:")
         self.input1 = QLineEdit()
 
-        self.label2 = QLabel("Passwort:")
+        self.label2 = QLabel("password:")
         self.input2 = QLineEdit()
         self.input2.setEchoMode(QLineEdit.Password)
 
-        self.submit_btn = QPushButton("Speichern")
+        self.submit_btn = QPushButton("Save")
         self.submit_btn.clicked.connect(self.saveData)
 
         layout.addWidget(self.label1)
@@ -34,7 +34,7 @@ class User_Form(QWidget):
         self.setLayout(layout)
 
     def initDB(self):
-        """ Tabelle erstellen, falls nicht vorhanden """
+        """ Create Table if not exists """
         self.conn = sqlite3.connect("sales.db")
         self.cursor = self.conn.cursor()
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS user_pass (
@@ -50,12 +50,12 @@ class User_Form(QWidget):
         if user_name and password:
             self.cursor.execute("INSERT INTO user_pass (user_name, password) VALUES (?, ?)", (user_name, password))
             self.conn.commit()
-            print("Daten erfolgreich gespeichert")
+            print("Data saved successfully!")
 
             self.input1.clear() 
             self.input2.clear()  
         else:
-            print("Bitte geben Sie Ihren Benutzernamen und Ihr Passwort ein.!")
+            print("Please enter your username and password!")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
